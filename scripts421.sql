bd1e1fd3aec6ba3825b2a0e5fdc5571bad6b1bd6
11th <11th@bk.ru>
2023-02-05 19:21:54 +0300

    4.2 corrections

--- scripts421.sql
+++ scripts421.sql
@@ -1,12 +1,12 @@
 create table student(
-    id bigserial primary key ,
+    id bigserial primary key,
     name varchar(100) not null,
     age integer check (age >= 16) default 20,
     faculty_id bigint references faculty (id)
 );
 
 create table faculty(
-    id bigserial primary key ,
+    id bigserial primary key,
     name varchar(100),
     color varchar(50)
 );
